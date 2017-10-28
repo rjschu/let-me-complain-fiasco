3dee316392023460df0513e3720870923d2bd23b
conor.toner <conor.toner@autotrader.co.uk>
2017-10-28 21:34:41 +0100

    checking if i can push

--- src/main/webapp/src/app/newComplaint/new-complaint.component.ts
+++ src/main/webapp/src/app/newComplaint/new-complaint.component.ts
@@ -3,8 +3,17 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'new-complaint',
-    templateUrl: './new-complaint.html'
+    templateUrl: './new-complaint.html',
+    styleUrls: ['./new-complaint.component.css']
 })
 export class NewComplaintComponent {
+
+    typesOfComplaint = [
+        "Letting of Steam",
+        "Just Angry",
+        "The bad person said mean things to me",
+        "Bored, so you decided to make a fake complaint",
+        "Genuine Complaint"
+    ]
 
 }
