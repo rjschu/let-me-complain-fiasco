80af9a43ea319464a7bb072c2f49b94d47c7a67a
alice.montgomery <alice.montgomery@autotrader.co.uk>
2017-10-28 17:18:49 +0100

    We have style now

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -2,8 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatCheckboxModule } from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+      BrowserAnimationsModule,
+      MatButtonModule,
+      MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
