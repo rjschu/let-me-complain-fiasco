5876a6f50859a0d73f82ecb230ea6d9b338251e4
CToner3223 <conor.toner@autotrader.co.uk>
2017-10-29 09:33:36 +0000

    baby grew up

--- src/main/webapp/src/app/reliant-captcha/reliant-captcha.component.ts
+++ src/main/webapp/src/app/reliant-captcha/reliant-captcha.component.ts
@@ -8,13 +8,26 @@ import {MatDialogRef} from "@angular/material";
 })
 export class ReliantCaptchaComponent implements OnInit {
 
+  selectedImages: boolean[] = [];
+
   constructor(public dialogRef: MatDialogRef<ReliantCaptchaComponent>) { }
 
   ngOnInit() {
   }
 
-  submitCaptcha(){
-    this.dialogRef.close(true);
+  selectImage(correctImage : boolean){
+
+    this.selectedImages.push(correctImage);
+
+    if(this.selectedImages.length == 3){
+      if(this.selectedImages[0] == true && this.selectedImages[1] == true && this.selectedImages[2] == true){
+        this.dialogRef.close(true);
+      }
+      else{
+        this.dialogRef.close(false);
+      }
+    }
+
   }
 
 }
